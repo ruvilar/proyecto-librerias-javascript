11c6bb8a435098aac1feb73f8b3c63bc098183bb
ruvilar <tecnobtc@gmail.com>
2022-08-10 20:29:44 -0300

    Fase inicial terminada

--- moduloFinal.js
+++ moduloFinal.js
@@ -6,12 +6,22 @@ axios.get(apiUrl)
 
 const fillTasks = data => {
     data.map(val => {
-        let newTask = document.createElement('article')
-        let taskTitle = document.createElement('h4')
-        taskTitle.innerHTML = val.title
-        newTask.appendChild(taskTitle)
+        let newTask = document.createElement('article'),
+            taskTitle = document.createElement('h4'),
+            taskDescription = document.createElement('p')
 
-        let column = document.getElementById('toDoTasks')
-        column.appendChild(newTask)
-    })
-}
+        taskTitle.innerHTML = `${val.title}<hr>`;
+        taskDescription.innerHTML = `<b>Responsable:</b> ${val.responsable}<br> <b>Estado:</b> ${val.state}<br> <b>Fecha Límite:</b> ${val.deadline}<br> <b>Fecha de Creación:</b> ${val.created}`;
+
+        newTask.appendChild(taskTitle);
+        newTask.appendChild(taskDescription);
+
+        let columnToDo = document.getElementById('toDoTasks'),
+            columnInProgress = document.getElementById('inProgressTasks'),
+            columnDone = document.getElementById('doneTasks')
+
+        if(val.state === 'TO DO') {columnToDo.appendChild(newTask)};
+        if(val.state === 'IN PROGRESS') {columnInProgress.appendChild(newTask)};
+        if(val.state === 'DONE') {columnDone.appendChild(newTask)};
+    });
+};
